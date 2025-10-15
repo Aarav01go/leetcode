6da5aabd22e21253d54495ae6b3964ebfad49ac4
Aarav <aaravg901@gmail.com>
2025-10-15 12:39:43 +0530

    Time: 0 ms (100%), Space: 28.3 MB (7.67%) - LeetHub

---
+++ 0169-majority-element/0169-majority-element.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int majorityElement(vector<int>& nums) {
+        unordered_map<int,int>o;
+        for(int &x : nums){
+            o[x]++;
+        }
+        int mx = INT_MIN;
+        int maxFreq = INT_MIN;
+        for(auto x : o){
+            int first = x.first;
+            int second = x.second;
+            if(second>maxFreq){
+                mx = first;
+                maxFreq = second;
+            }
+        }
+        return mx;
+        
+    }
+};
