7d4efc1fcb9f9ad9c0608856cb1427c4aaa4eb11
Aarav <aaravg901@gmail.com>
2025-12-24 17:09:49 +0530

    Time: 3 ms (55.41%), Space: 16.1 MB (10.68%) - LeetHub

--- 0784-letter-case-permutation/0784-letter-case-permutation.cpp
+++ 0784-letter-case-permutation/0784-letter-case-permutation.cpp
@@ -2,20 +2,24 @@ class Solution {
 public:
     vector<string>result;
     void solve(string s, int i, string temp){
-        if(i>=s.size()){
-            for(int j=0;j<result.size();j++){
-                if(temp==result[j]) return;
-            }
+        if (i == s.size()) {
             result.push_back(temp);
             return;
         }
-        temp.push_back(tolower(s[i]));
-        solve(s,i+1,temp);
-        temp.pop_back();
-        temp.push_back(toupper(s[i]));
-        solve(s,i+1,temp);
 
+        if (isalpha(s[i])) {
+            temp.push_back(tolower(s[i]));
+            solve(s, i + 1, temp);
+            temp.pop_back();
 
+            temp.push_back(toupper(s[i]));
+            solve(s, i + 1, temp);
+            temp.pop_back();
+        } else {
+            temp.push_back(s[i]);
+            solve(s, i + 1, temp);
+            temp.pop_back();
+        }
     }
     vector<string> letterCasePermutation(string s) {
         string temp;
