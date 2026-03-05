8dd36bc99f621bc5760555bd1819eae58ca93a36
Aarav <aaravg901@gmail.com>
2026-03-05 19:15:12 +0530

    Time: 0 ms (100%), Space: 9.5 MB (51.31%) - LeetHub

---
+++ 1758-minimum-changes-to-make-alternating-binary-string/1758-minimum-changes-to-make-alternating-binary-string.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int minOperations(string s) {
+        int cnt=0;
+        for(int i=0;i<s.size()-1;i++){
+            if(s[i]==s[i+1]){
+                cnt++;
+                if(s[i]=='1') s[i+1]='0';
+                else s[i+1]='1';
+            }
+        }
+        return min(cnt, (int)s.size() - cnt);
+    }
+};
