6621f696d6dcec7cff291eecdbd41d6d81340dd4
Aarav <aaravg901@gmail.com>
2026-03-21 23:04:12 +0530

    Time: 20 ms (14.04%), Space: 58.2 MB (59.53%) - LeetHub

---
+++ 1642-furthest-building-you-can-reach/1642-furthest-building-you-can-reach.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int furthestBuilding(vector<int>& heights, int bricks, int ladders) {
+        priority_queue<int, vector<int>, greater<int>> minHeap;
+        for (int i = 0; i < heights.size() - 1; i++) {
+            int jump = heights[i + 1] - heights[i];
+            if (jump <= 0)
+                continue;
+            minHeap.push(jump);
+            if (minHeap.size() > ladders) {
+                bricks -= minHeap.top();
+                minHeap.pop();
+            }
+            if (bricks < 0)
+                return i;
+        }
+        return heights.size() - 1;
+    }
+};
