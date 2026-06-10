64dd12a6f6ee2e5e5db73101c7e2737b4034d87a
Aarav Gupta <aaravg901@gmail.com>
2026-06-10 22:34:53 +0530

    Time: 12 ms (22.04%), Space: 14.3 MB (27.76%) - LeetHub

---
+++ 0006-zigzag-conversion/0006-zigzag-conversion.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    string convert(string s, int numRows) {
+        if (numRows <= 1 || numRows >= s.length()) return s;
+
+        vector<string> rows(numRows);
+        int row = 0;
+        int step = 1;
+
+        for (char c : s) {
+            rows[row] += c;
+            
+            if (row == 0) step = 1;
+            else if (row == numRows - 1) step = -1;
+            
+            row += step;
+        }
+
+        string result = "";
+        for (string r : rows) {
+            result += r;
+        }
+
+        return result;
+    }
+};
