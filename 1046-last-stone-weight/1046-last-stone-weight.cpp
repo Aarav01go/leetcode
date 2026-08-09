7d24fbd10793e262c0777797a1de06d53c0eb496
Aarav Gupta <aaravg901@gmail.com>
2026-08-09 13:04:41 +0530

    Time: 0 ms (100%), Space: 9.9 MB (80.34%) - LeetHub

--- 1046-last-stone-weight/1046-last-stone-weight.cpp
+++ 1046-last-stone-weight/1046-last-stone-weight.cpp
@@ -1,29 +1,14 @@
 class Solution {
 public:
     int lastStoneWeight(vector<int>& stones) {
-        while (stones.size() > 1) {
-            int m1 = INT_MIN, m2 = INT_MIN;
-            int a = -1, b = -1;
-            for (int j = 0; j < (int)stones.size(); j++) {
-                if (stones[j] >= m1) {
-                    m2 = m1;
-                    b = a;
-                    m1 = stones[j];
-                    a = j;
-                } else if (stones[j] > m2) {
-                    m2 = stones[j];
-                    b = j;
-                }
-            }
-            if (a > b) {
-                stones.erase(stones.begin() + a);
-                stones.erase(stones.begin() + b);
-            } else {
-                stones.erase(stones.begin() + b);
-                stones.erase(stones.begin() + a);
-            }
-            if (m1 != m2) stones.push_back(m1 - m2);
+        priority_queue<int> pq(stones.begin(), stones.end());
+        while (pq.size() > 1) {
+            int m1 = pq.top();
+            pq.pop();
+            int m2 = pq.top();
+            pq.pop();
+            if (m1 != m2) pq.push(m1 - m2);
         }
-        return stones.empty() ? 0 : stones[0];
+        return pq.empty() ? 0 : pq.top();
     }
 };
