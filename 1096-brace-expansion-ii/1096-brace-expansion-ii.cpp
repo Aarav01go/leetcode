4f1089d8e3a74e5090d4bec2903c7c1fb16e9067
Aarav Gupta <aaravg901@gmail.com>
2026-09-25 23:22:27 +0530

    Time: 16 ms (14.73%), Space: 18.4 MB (12.95%) - LeetHub

---
+++ 1096-brace-expansion-ii/1096-brace-expansion-ii.cpp
@@ -0,0 +1,61 @@
+class Solution {
+public:
+    int n;
+    string s;
+    int idx=0;
+
+    set<string>getUnit(){
+        set<string> result;
+
+        if(s[idx]=='{'){
+            idx++;
+            result=performUnion();
+        }
+        else {
+            result ={string(1,s[idx])};
+        }
+        idx++;
+        return result;
+    }
+
+    set<string> PerformConcat(){
+        set<string> result={""};
+
+        while(idx<n&&s[idx]=='{'||isalpha(s[idx])){
+            set<string>temp =getUnit();
+
+            set<string> ConcatResult;
+            for(const string& left: result){
+                for(const string& right :temp){
+                    ConcatResult.insert(left+right);
+                }  
+            }
+            result=ConcatResult;
+        }
+        return result;
+    }
+
+    set<string> performUnion(){
+        set<string > result;
+
+        while(true){
+            set<string> temp =PerformConcat();
+            result.insert(begin(temp),end(temp));
+
+            if(idx<n&& s[idx]==',') idx++;
+            else break;
+        }
+        return result;
+    }
+
+    vector<string> braceExpansionII(string expression) {
+        s=expression;
+        n=s.size();
+        idx=0;
+
+        set<string> st=performUnion();
+        vector<string> result (st.begin(),st.end());
+
+        return result;
+    }
+};
